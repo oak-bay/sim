e724baf9c2b4ac2f9be40f99556d4a1323b88158
oak-bay <liuyang4pub@sina.com>
2020-10-10 06:23:10 +0800

    fix: EventScheduler on times-mode

--- sim/scheduler.py
+++ sim/scheduler.py
@@ -5,7 +5,15 @@ from sim import Environment
 
 
 class EventScheduler:
-    """ 事件调度器. """
+    """ 事件调度器.
+
+    Usages:
+        # env = Environment()
+        # fun(env)
+        env.step_events.append(EventScheduler(evt=fun, times=[1, 3])) # call at 1s, 3s
+        env.step_events.append(EventScheduler(evt=fun, dt=0.2)) # call every 0.2s
+        env.step_events.append(EventScheduler(evt=fun, rand=[1, 3])) # call at random interval between [1s, 3s]
+    """
 
     class TimerType(Enum):
         UNKNOWN = 0
@@ -15,13 +23,19 @@ class EventScheduler:
 
     def __init__(self, evt=None, **kwargs):
         """ 初始化.
+
+        :param evt: 事件处理函数.
+        :param dt: 按照固定事件间隔调用事件.
+        :param times: 按照规定的事件序列调用事件.
+        :param rand: 按照随机的事件间隔调用事件.
         """
         self.evt = evt
         self.type = EventScheduler.TimerType.UNKNOWN
         self.dt = None
         self.rng = None
         self.times = None
-        self._check_pt = None
+        self._check_pt = None  # 当前检查时间点（用于启动事件）
+        self._env = None
         self.set_params(**kwargs)
 
     def set_params(self, **kwargs):
@@ -36,10 +50,10 @@ class EventScheduler:
             self.type = EventScheduler.TimerType.TIMES
 
     def __call__(self, env: Environment):
+        self._env = env
         now, _ = env.time_info
-        if self._check_and_update(now, env):
-            if self.evt:
-                self.evt(env)
+        if self._check_and_update(now, env) and self.evt:
+            self.evt(env)
 
     def __rand_time(self):
         return random.random() * (self.rng[1] - self.rng[0]) + self.rng[0]
@@ -48,20 +62,29 @@ class EventScheduler:
         if self.type == EventScheduler.TimerType.FIX:
             if self._check_pt is None:
                 self._check_pt = env.clock_info[0]
-            if self._check_pt is not None and now >= self._check_pt:
+            if self._check_now(now):
                 self._check_pt += self.dt
                 return True
         if self.type == EventScheduler.TimerType.RAND:
             if self._check_pt is None:
                 self._check_pt = env.clock_info[0] + self.__rand_time()
-            if self._check_pt is not None and now >= self._check_pt:
+            if self._check_now(now):
                 self._check_pt += self.__rand_time()
                 return True
         if self.type == EventScheduler.TimerType.TIMES:
             if self._check_pt is None and len(self.times) > 0:
                 self._check_pt = self.times[0]
-            if self._check_pt is not None and now >= self._check_pt:
+            if self._check_now(now):
                 idx = self.times.index(self._check_pt) + 1
                 self._check_pt = self.times[idx] if idx < len(self.times) else None
                 return True
         return False
+
+    def _check_now(self, now):
+        assert self._env is not None
+        if self._check_pt is not None:
+            _, dt = self._env.time_info
+            if dt > now - self._check_pt >= 0.0:
+                return True
+        return False
+
